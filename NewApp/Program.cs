aeb0f3efeb12a67a7605076563c1c3c061c5c7cb
NguyenHuyTuong2021050718 <Ngongtunho.20000@gmail.com>
2023-09-07 18:11:48 +0700

    CreateProject

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,6 +1,8 @@
 ﻿
-
-//in ra màn hình a + b = 12
-int a = 9;
-int b = 3;
-Console.WriteLine("{0} + {1} = {2}", a, b, a+b);
+Console.WriteLine("Hello, World!");
+//ví dụ
+//in ra màn hình: 6
+Console.WriteLine(6);
+// in ra màn hình : a = 9
+int a = 9; //khai bao biến số a kiểu int vfaf gán giá trị = 9
+Console.WriteLine("a = " + a);
