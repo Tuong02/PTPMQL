1300f589880ca1db49bdd4d4f45b59a48956bf5c
NguyenHuyTuong2021050718 <Ngongtunho.20000@gmail.com>
2023-09-07 18:06:59 +0700

    CreateProject

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 Console.WriteLine("Hello, World!");
+//ví dụ
+//in ra màn hình: 6
+Console.WriteLine(6);
+// in ra màn hình : a = 9
+int a = 9; //khai bao biến số a kiểu int vfaf gán giá trị = 9
+Console.WriteLine("a = " + a);
+
+
