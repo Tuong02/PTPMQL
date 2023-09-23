378141684f26ff67785159adbd2b65bc0582e4ae
NguyenHuyTuong2021050718 <Ngongtunho.20000@gmail.com>
2023-09-23 20:24:55 +0700

    CreateProject

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -3,6 +3,13 @@
  System.Console.WriteLine("Du lieu nhap vao la: ");
 ps.Nhapthongtin();
 ps.Hienthi();
+Student std = new Student();
+std.NhapThongTin();
+std.HienThi();
+Fruit frt = new Fruit();
+frt.NhapThongTin();
+frt.HienThi();
+
 
 
 
