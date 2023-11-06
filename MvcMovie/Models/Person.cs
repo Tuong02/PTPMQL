734b877f893b3f57dbec316a74adc35278c1a7d6
NguyenHuyTuong2021050718 <Ngongtunho.20000@gmail.com>
2023-11-06 13:37:13 +0700

    CRUD du lieu

--- MvcMovie/Models/Person.cs
+++ MvcMovie/Models/Person.cs
@@ -7,7 +7,7 @@ namespace MvcMovie.Models
     public class Person
     {
          [Key]
-        public string PersonID { get; set; }
+        public string PersonId { get; set; }
         public string FullName { get; set; }
         public string Address { get; set; }   
     }
